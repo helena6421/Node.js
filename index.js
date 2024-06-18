078efbef48fc18309fbf1703c033790f831b3b0f
helena6421 <40118858+helena6421@users.noreply.github.com>
2024-06-18 17:12:02 +0300

    Добавлены файлы error404, logger, добавлена ошибка 500

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ app.use(express.json())
 
 const router = require('./router')
 
+const logger = require('./multer/logger')
+const error404 = require('./multer/error-404')
+
 app.use('/public', express.static(__dirname+'/public'))
 app.use('/api', router)
+
+app.use(logger)
+app.use(error404)
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('500 | Ошибка сервера')
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
